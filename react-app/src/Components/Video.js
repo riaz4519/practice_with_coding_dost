869ab7f5acadeff04bb4a8cc13097caabab34b08
riaz4519 <riaz.i3216@gmail.com>
2023-12-19 20:10:08 +0600

    working with react events now

--- react-app/src/Components/Video.js
+++ react-app/src/Components/Video.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import './Video.css'
 
-function Video({ title, channel, views, time,thumb }) {
+function Video({
+  title,
+  channel = 'Coder Dost',
+  views,
+  time,
+  id,
+  verified = true,
+}) {
   return (
     <>
       <div className="container">
         <div className="pic">
-          <img src={thumb} alt="Katherine Johnson" />
+          <img
+            src={`https://picsum.photos/id/${id}/100/100`}
+            alt="Katherine Johnson"
+          />
         </div>
 
         <div className="title">{title}</div>
-        <div className="channel">{channel}</div>
+        <div className="channel">
+          {channel} {verified && '✅'}
+        </div>
+
         <div className="views">
           {views} views <span>.</span>
           {time}
---
+++ react-app/src/data/data.js
@@ -0,0 +1,28 @@
+let videos = [
+  {
+    title: 'React Js Tutorial',
+    views: '10k',
+    time: '1 year ago',
+    channel: 'Coder Dost',
+    id: 1,
+    verified: true,
+  },
+  {
+    title: 'Node Js Tutorial',
+    views: '100k',
+    time: '1 month ago',
+    channel: 'Coder Dost',
+    id: 7,
+    verified: false,
+  },
+  {
+    title: 'React Js Tutorial',
+    views: '10k',
+    time: '1 year ago',
+    channel: 'Coder Dost',
+    id: 202,
+    verified: true,
+  },
+]
+
+export default videos;

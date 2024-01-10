eeb55499323f3e9f75560adeef76f15e22e4257d
riaz4519 <riaz.i3216@gmail.com>
2024-01-10 19:35:55 +0600

    on RTK query

--- react-wtih-redux-toolkit-3/src/slices/accountSlice.js
+++ react-wtih-redux-toolkit-3/src/slices/accountSlice.js
@@ -8,7 +8,6 @@ const initialState = {
 export const getUserAccount = createAsyncThunk(
   'account/createUser',
   async (userId, thunkApi) => {
-
     const { data } = await axios.get(`http://localhost:8080/accounts/${userId}`)
 
     return data.amount
@@ -47,54 +46,3 @@ export const accountSlice = createSlice({
 export const { increment, decrement, incrementByAmount } = accountSlice.actions
 
 export default accountSlice.reducer
-
-// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import axios from 'axios';
-
-// const initialState = {
-//   amount: 10,
-// };
-
-// export const getUserAccount = createAsyncThunk(
-//   'account/getUser',
-//   async (userId, thunkAPI) => {
-//     const { data } = await axios.get(
-//       `http://localhost:8080/accounts/${userId}`
-//     );
-//     return data.amount;
-//   }
-// );
-
-// export const accountSlice = createSlice({
-//   name: 'account',
-//   initialState,
-//   reducers: {
-//     increment: (state) => {
-//       state.amount += 1; //immer
-//     },
-//     decrement: (state) => {
-//       state.amount -= 1;
-//     },
-//     incrementByAmount: (state, action) => {
-//       state.amount += action.payload;
-//     },
-//   },
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(getUserAccount.fulfilled, (state, action) => {
-//         state.amount = action.payload;
-//         state.pending = false;
-//       })
-//       .addCase(getUserAccount.pending, (state, action) => {
-//         state.pending = true;
-//       })
-//       .addCase(getUserAccount.rejected, (state, action) => {
-//         state.error = action.error;
-//       });
-//   },
-// });
-
-// // Action creators are generated for each case reducer function
-// export const { increment, decrement, incrementByAmount } = accountSlice.actions;
-
-// export default accountSlice.reducer;
